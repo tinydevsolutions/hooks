c64655aa05ce7c2e0c87d3ae343043871eb75832
tinydevsolutions <tinydevsolutions@gmail.com>
2025-12-10 15:15:15 +0530

    added new things

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,20 @@ import Login from "./hooks/useReducer/Login";
 import SearchList from "./hooks/useMemo/SearchList";
 import ProductSearch from "./hooks/useMemo/Search";
 import InputFocus from "./hooks/useRef/Input";
+import Nav from "./components/Nav";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div className="flex flex-col justify-center items-center mx-auto">
+      {/* <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>  */}
+
       <State />
       <SideEffects />
       <RenderCounter />
